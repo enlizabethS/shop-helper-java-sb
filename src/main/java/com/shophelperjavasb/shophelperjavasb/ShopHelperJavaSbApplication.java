d3daad35fd20c0ac4411224fbc00371465a02b74
endorphinchik <elisaveta.stepura@mail.ru>
2023-06-17 23:05:22 +0300

    Initial commit

---
+++ src/main/java/com/shophelperjavasb/shophelperjavasb/ShopHelperJavaSbApplication.java
@@ -0,0 +1,13 @@
+package com.shophelperjavasb.shophelperjavasb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopHelperJavaSbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShopHelperJavaSbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shophelperjavasb/shophelperjavasb/ShopHelperJavaSbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shophelperjavasb.shophelperjavasb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopHelperJavaSbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
